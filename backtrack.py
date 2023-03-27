48e6ac351dbe9f1a5c43cf8e17ccb25ce3a7e30c
Chibi-girl <lucysanzenin@gmail.com>
2023-03-27 14:13:01 +0530

    updated backtracking code

--- backtrack.py
+++ backtrack.py
@@ -1,26 +1,49 @@
 from crossbar_simulator import *
 
-newCrossbar = crossbar_simulator(5,5)
 # choose all memristors
 # for each memristor test all paths containing memristor
 # if any path returns ideal current then consideration is wrong
 # backtrack and check some other memristor
-def locate_faulty_memristor(m,n):
-	for i in range (0,n):
-		for j in range (0,m):
-			fault = 1
-			for a in range (0,n):
-				fault = newCrossbar.detect_current(a,j)
-				if(fault == 2) :
-					break
-			if fault == 2:
-				continue
-			for b in range (0,m):
-				fault = newCrossbar.detect_current(i,b)
-				if(fault == 2):
-					break
-			if fault == 2:
-				continue
-			return (i,j)
-#print(newCrossbar._crossbar_simulator__faulty_memristor)
-print(locate_faulty_memristor(5,5))
+
+class backtack:
+	def __init__(self, m, n):
+		self.m = m
+		self.n = n
+		self.detected_faulty_wordline=-1
+		self.detected_faulty_bitline=-1
+
+	def locate_faulty_memristor(self):
+		m = self.m
+		n = self.n
+		simul = crossbar_simulator(5,5)
+
+		for i in range (0,n):
+			for j in range (0,m):
+				
+				fault = 1
+				
+				for a in range (0,n):
+					fault = simul.detect_current(a,j)
+					if(fault == 2) :
+						break
+				if fault == 2:
+					continue
+				
+				for b in range (0,m):
+					fault = simul.detect_current(i,b)
+					if(fault == 2):
+						break
+				if fault == 2:
+					continue
+
+				self.detected_faulty_wordline = i
+				self.detected_faulty_bitline = j
+				if(simul.verify_fault([self.detected_faulty_wordline,self.detected_faulty_bitline])):
+					print(f"Fault found at [{self.detected_faulty_wordline},{self.detected_faulty_bitline}] and verified successfully")
+				else:
+					print(f"Fault found at [{self.detected_faulty_wordline},{self.detected_faulty_bitline}] and not verified successfully")
+				
+				return
+
+temp = backtack(5,5)
+temp.locate_faulty_memristor()
